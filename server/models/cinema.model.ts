f9619cc61579df31184deb0c01298664a8965b0e
Suriya Kumar, Prabhu <80642@global.ul.com>
2018-06-20 09:54:51 -0500

    updated model

--- server/models/cinema.model.ts
+++ server/models/cinema.model.ts
@@ -1,17 +1,23 @@
-import { Model } from "@tsed/mongoose";
-import { Schema } from "mongoose";
-import {Enum, IgnoreProperty, Pattern, Property, Required} from "@tsed/common";
+import { MongooseDocument } from '@tsed/mongoose/lib';
+import { Model } from '@tsed/mongoose';
+import { Schema } from 'mongoose';
+import { Enum, IgnoreProperty, Pattern, Property, Required } from '@tsed/common';
 
 @Model()
 export class Cinema {
-    @Property()
-    _id: string;
-    @Property()
-    movieName: string;
-    @Property()
-    directorName: string;
-    @Property()
-    runningLength: string;
-    @Property()
-    posterUrl: string;
+  @Property() _id: string;
+
+  @Property() name: string;
+  @Property() year: string;
+  @Property() genre: string[];
+  @Property() director: string;
+
+  @Property() posterUrl: string;
+  @Property() posterImgUrl: string;
+
+  @Property() synopsis: string;
+  @Property() imdbId: string;
+  @Property() imdbLink: string;
+  @Property() rating: string;
+  @Property() ratingCount: string;
 }
